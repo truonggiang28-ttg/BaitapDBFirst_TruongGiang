96727cc9cefe600e0ec8b63568d355a00fe8345b
Thai Truong Giang <truonggiang121604@gmail.com>
2026-04-10 21:15:45 +0700

    Tao database Employee

---
+++ Sql_Company_TruongGiang.sql
@@ -0,0 +1,13 @@
+﻿CREATE DATABASE CompanyDB;
+GO
+USE CompanyDB;
+GO
+CREATE TABLE Employees (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    FullName NVARCHAR(100) NOT NULL,
+    Position NVARCHAR(50) NOT NULL,
+    Salary DECIMAL(18,2) NOT NULL
+);
+INSERT INTO Employees (FullName, Position, Salary) VALUES 
+(N'Nguyễn Văn A', 'Developer', 1500), 
+(N'Trần Thị B', 'Designer', 1200);
